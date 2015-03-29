b6b24d4f7e5a6e08886111feb9db5c478919e22b
sagararathi <bravo.sagar@gmail.com>
2015-03-28 23:42:33 -0400

    Add action to question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,40 @@ class QuestionsController < ApplicationController
   def create
 
   end
+
+  def show
+    @question = Question.where(topic_id: params[:sub_topic]).first
+    p @answers
+    if request.xhr?
+      if @question
+        @answers = Answer.where(question_id: @question.id)
+        render :json => {:question => @question, :answers => @answers}
+      else
+        render json: {question: @question}
+      end
+    end
+  end
+
+  def next
+
+  ques = Question.find(params[:id])
+  @question = Question.where(topic_id: ques.topic_id).where("id > ?", ques.id).first
+    if request.xhr?
+      if @question
+        @answers = Answer.where(question_id: @question.id)
+        render :json => {question: @question, answers: @answers}
+      else
+        render json: {status: "No more Question"}
+      end
+    end
+  end
+
+  private
+  def display_parent_topic
+    @topics= Topic.where(parent_topic_id: nil)
+  end
+
+  def question_params
+    params.require(:question).permit(:ques, :topic_id)
+  end
 end
